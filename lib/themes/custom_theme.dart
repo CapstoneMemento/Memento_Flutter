28733c9d19532930abc8608bba2f2bcc0ea85484
Yongyeon Kim <yeonlotus@ewhain.net>
2023-01-14 11:35:46 +0900

    Style: custom theme 추가

---
+++ lib/themes/custom_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomTheme {
+  static ThemeData themeData = ThemeData(
+    primaryColor: const Color(0xFF4CA2DA),
+    textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w700,
+        ),
+        titleMedium: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
+        ),
+        titleSmall: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.w700,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w400,
+        ),
+        bodySmall: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w300,
+            color: Color(0xff828282))),
+  );
+}
