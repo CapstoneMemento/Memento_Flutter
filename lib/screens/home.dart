dfba4b02b236a53787ac95bf9ce3608392de98a1
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-02-25 22:08:23 +0900

    Feat: 네이버 OCR에서 응답 받기

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
+import "package:http/http.dart" as http;
 
 import 'package:document_scanner_flutter/configs/configs.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:memento_flutter/config/constants.dart';
 import 'package:memento_flutter/screens/subjectListScreen.dart';
 import 'package:memento_flutter/themes/custom_theme.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
@@ -28,6 +31,7 @@ class _HomeState extends State<Home> {
   late File scannedImage; // 스캔한 이미지
 
 /* Firebase storage 초기화 */
+
   final storageRef = FirebaseStorage.instance.ref();
   late final scannedImageRef = storageRef.child("scannedImage.jpg");
 
@@ -89,7 +93,7 @@ class _HomeState extends State<Home> {
 
   /* '사진 촬영하기' 선택 시, 문서 스캐너 실행 */
   openImageScanner(BuildContext context) async {
-    final image = await DocumentScannerFlutter.launch(context,
+    var image = await DocumentScannerFlutter.launch(context,
         source: ScannerFileSource.CAMERA);
 
     // 촬영한 이미지 저장
@@ -102,24 +106,46 @@ class _HomeState extends State<Home> {
 
 /* '앨범에서 가져오기' 선택 시 실행하는 함수 */
   runFilePicker(BuildContext context) async {
-    final image = await DocumentScannerFlutter.launch(context,
+    // 앨범 실행
+    var image = await DocumentScannerFlutter.launch(context,
         source: ScannerFileSource.GALLERY);
 
     // 앨범에서 선택한 이미지 저장
     if (image != null) {
       scannedImage = image;
 
+      // Firebase storage에 이미지 저장
       try {
-        await scannedImageRef.putFile(scannedImage); // Firebase storage에 이미지 저장
-        final downloadURL =
-            await scannedImageRef.getDownloadURL(); // image URL 가져오기
-
+        await scannedImageRef.putFile(scannedImage);
       } catch (error) {
         log(error.toString());
       }
+      // image URL 가져오기
+      var downloadURL = await scannedImageRef.getDownloadURL();
+      runOCR(downloadURL);
+    }
+  }
 
-      // 네이버 OCR로 텍스트 추출
-      setState(() {});
+  /* 네이버 OCR 실행 */
+  void runOCR(String imageURL) async {
+    var url = Uri.parse(Constants.invokeURL); // url을 uri로 변환
+    var headers = {"X-OCR-SECRET": Constants.secretKey};
+    var body = {
+      "images": [
+        {"format": "jpeg", "name": "note", "data": null, "url": imageURL}
+      ],
+      "lang": "ko",
+      "requestId": "string",
+      "timestamp": 0,
+      "version": "V2",
+      "enableTableDetection": false
+    }; // images 안에 List 때문에 object로 인식되지 않으므로, jsonEncode 사용
+    try {
+      var response =
+          await http.post(url, headers: headers, body: jsonEncode(body));
+      // print(jsonDecode(response.body)); // 응답 확인 완료
+    } catch (error) {
+      // print(error);
     }
   }
 
