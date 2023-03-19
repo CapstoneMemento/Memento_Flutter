26b3dd30fda973b12a7122ae96abffdcaadbec7b
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-03-19 17:21:37 +0900

    Feat: 기존 밑줄 포함하면 삭제하고 새로 추가

--- lib/screens/keyword_select_screen.dart
+++ lib/screens/keyword_select_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:memento_flutter/screens/title_setting_screen.dart';
 import 'package:memento_flutter/themes/custom_theme.dart';
 import 'package:memento_flutter/widgets/base_app_bar.dart';
+import 'package:memento_flutter/widgets/navigation_bar.dart';
 
 class KeywordSelectScreen extends StatefulWidget {
   final String noteId;
@@ -20,17 +21,16 @@ class _KeywordSelectScreenState extends State<KeywordSelectScreen> {
   List<Map<String, dynamic>> selectedIndex = [];
   // 선택한 문자 {text, isKeyword}
   List<Map<String, dynamic>> selectedText = [];
-  // 선택한 문자 text style
+  // 선택한 문자 TextStyle
   final highlightStyle =
       TextStyle(backgroundColor: Colors.yellow.withOpacity(0.5));
 
   /* 선택한 문자와 그러지 않은 문자를 나눠서 배열에 저장 */
   List<Map<String, dynamic>> sliceText(
       String totalText, List<Map<String, dynamic>> selectedIndex) {
-    List<Map<String, dynamic>> result = []; // [String text, bool highlighted]
-
+    List<Map<String, dynamic>> result = []; // 선택한 문자 {text, isKeyword}
     int prevEndIndex = 0; // 이전 end index
-    /* 선택한 text이면 true, 아니면 false를 String과 함께 저장 */
+
     for (final index in selectedIndex) {
       final start = index["start"];
       final end = index["end"];
@@ -44,35 +44,35 @@ class _KeywordSelectScreenState extends State<KeywordSelectScreen> {
     }
     // 마지막 문자 저장
     result.add({"text": totalText.substring(prevEndIndex), "isKeyword": false});
-    selectedText = result; // 선택한 문자 저장
+    selectedText = result;
 
     return result;
   }
 
+  /* 사용자가 키워드를 선택하면 index 저장 */
   void _onSelectionChanged(selection, cause) {
-    /* 사용자가 키워드를 선택하면 index 저장 */
     if (cause == SelectionChangedCause.longPress ||
         cause == SelectionChangedCause.drag) {
       saveIndex(selection.baseOffset, selection.extentOffset);
-      // 오름차순 정렬 (사용자가 )
+      // 오름차순 정렬 (사용자가 순서대로 밑줄을 긋지 않을 경우에 대비)
       sortIndex(selectedIndex);
       setState(() {});
     }
   }
 
   void saveIndex(int newStartIndex, int newEndIndex) {
-    /* 시작 인덱스가 동일하고
-      끝 인덱스가 기존 끝 인덱스보다 크면,
-      최근 거로 대체 (드래그해서 여러 단어를 밑줄 그을 경우) */
-    var isSaveNew = true;
+    var isSaveNew = true; // 인덱스 새로 저장?
 
     for (var i = 0; i < selectedIndex.length; i++) {
       final start = selectedIndex[i]["start"]; // 시작 인덱스
       final end = selectedIndex[i]["end"]; // 끝 인덱스
 
-      /* 인덱스 삭제 */
-      // 기존 밑줄을 다시 길게 누르면
-      if (newStartIndex >= start && newEndIndex <= end) {
+      final pressAgain = newStartIndex >= start && newEndIndex <= end;
+      final isDragging = newStartIndex == start && newEndIndex > end;
+      final include = newStartIndex < start && newEndIndex > start;
+
+      // 기존 밑줄을 다시 길게 누르면 삭제
+      if (pressAgain) {
         // 기존 인덱스 삭제
         selectedIndex.removeAt(i);
         // 새 인덱스 저장 X
@@ -81,12 +81,16 @@ class _KeywordSelectScreenState extends State<KeywordSelectScreen> {
       }
 
       // 사용자가 밑줄 긋는 중이면
-      if (start == newStartIndex && end < newEndIndex) {
+      if (isDragging) {
         // 기존 끝 인덱스 갱신
         selectedIndex[i]["end"] = newEndIndex;
-        // 새 인덱스 저장 X
         isSaveNew = false;
-        break;
+      }
+
+      // 기존 밑줄을 포함하면 삭제
+      if (include) {
+        selectedIndex.removeAt(i);
+        isSaveNew = false;
       }
     }
 
@@ -102,26 +106,35 @@ class _KeywordSelectScreenState extends State<KeywordSelectScreen> {
 
   /* 오름차순 정렬 */
   void sortIndex(List<Map<String, dynamic>> index) {
-    selectedIndex.sort(((a, b) {
+    selectedIndex.sort((a, b) {
       if (a["start"] == b["start"]) {
         return a["end"].compareTo(b["end"]);
       }
       return a["start"].compareTo(b["start"]);
-    }));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const BaseAppBar(
-        leading: Icon(
-          Icons.arrow_back_ios,
+      appBar: BaseAppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios),
           color: Colors.black,
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
         actions: [
-          Icon(
-            Icons.close,
+          IconButton(
+            icon: const Icon(Icons.close),
             color: Colors.black,
+            onPressed: () {
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(
+                      builder: (context) => const NavigationBarWidget()),
+                  (route) => false);
+            },
           ),
         ],
       ),
