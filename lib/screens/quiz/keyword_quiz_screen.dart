ee43b52c0c176b2acdea40900fdfe1edeb74999a
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-03-27 22:53:40 +0900

    Chore: 문자 대신 index로 빈칸 표시하기

--- lib/screens/quiz/keyword_quiz_screen.dart
+++ lib/screens/quiz/keyword_quiz_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:memento_flutter/screens/quiz/sentence_quiz_screen.dart';
 import 'package:memento_flutter/themes/custom_theme.dart';
 import 'package:memento_flutter/widgets/back_icon_button.dart';
 import 'package:memento_flutter/widgets/base_app_bar.dart';
@@ -12,36 +11,24 @@ class KeywordQuizScreen extends StatelessWidget {
     const title = "발명의 완성";
     const content =
         "특허권침해소송의 상대방이 제조 등을 하는 제품 또는 사용하는 방법(이하 ‘침해제품 등’이라고 한다)이 특허발명의 특허권을 침해한다고 하기 위해서는 특허발명의 특허청구범위에 기재된 각 구성요소와 그 구성요소 간의 유기적 결합관계가 침해제품 등에 그대로 포함되어 있어야 한다. 침해제품 등에 특허발명의 특허청구범위에 기재된 구성 중 변경된 부분이 있는 경우에도, 특허발명과 과제 해결원리가 동일하고, 특허발명에서와 실질적으로 동일한 작용효과를 나타내며, 그와 같이 변경하는 것이 그 발명이 속하는 기술분야에서 통상의 지식을 가진 사람이라면 누구나 쉽게 생각해 낼 수 있는 정도라면, 특별한 사정이 없는 한 침해제품 등은 특허발명의 특허청구범위에 기재된 구성과 균등한 것으로서 여전히 특허발명의 특허권을 침해한다고 보아야 한다.";
-    // *** 실제로는 index로 구현해야 함. keyword_select_screen에서 sliceText 참고 ***
-    // 띄어쓰기도 한 단어로 분리해야 함
-    const keywords = [
-      "특허권침해소송",
-      "침해제품",
-      "특허발명",
-      "특허청구범위",
-      "구성요소",
-      "유기적",
-      "결합관계",
-      "변경된",
-      "부분",
-      "과제",
-      "해결원리",
-      "작용효과",
-      "기술분야",
-      "통상의",
-      "지식",
-      "균등한",
-      "것",
-      "특허권",
-      "침해"
+    const selectedIndex = [
+      {"start": 0, "end": 7, "noteId": "123"},
+      {"start": 18, "end": 22, "noteId": "123"},
+      {"start": 55, "end": 59, "noteId": "123"},
+      {"start": 70, "end": 76, "noteId": "123"},
+      {"start": 84, "end": 88, "noteId": "123"},
+      {"start": 95, "end": 98, "noteId": "123"},
     ];
+
+    // 사용자 답안
+    List<String> userAnswerList = [];
 
     final ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
       backgroundColor: CustomTheme.themeData.primaryColor,
     );
 
-    /* 키워드 인덱스에 따라, 전체 문장을 나눠서 배열에 저장 */
-    List<dynamic> findKeyword(content, keywords) {
+    /* 문자 배열로 키워드가 주어질 경우 */
+    List<dynamic> findKeywords(content, keywords) {
       var result = []; // {text, isKeyword}
       var remainSentence = content;
 
@@ -53,7 +40,10 @@ class KeywordQuizScreen extends StatelessWidget {
         final keySentence =
             remainSentence.substring(startIndex, endIndex); // content의 키워드 부분
 
-        result.add({"text": prevSentence, "isKeyword": false});
+        // 빈 문자는 제외
+        if (prevSentence != "") {
+          result.add({"text": prevSentence, "isKeyword": false});
+        }
         result.add({"text": keySentence, "isKeyword": true});
         remainSentence = remainSentence.substring(endIndex);
       }
@@ -64,34 +54,64 @@ class KeywordQuizScreen extends StatelessWidget {
       return result;
     }
 
+    /* 키워드 인덱스에 따라, 전체 문장을 나눠서 배열에 저장 */
+    List<dynamic> sliceText(content, selectedIndex) {
+      var result = []; // {text, isKeyword}
+      var prevEndIndex = 0; // 이전 end index
+
+      for (final index in selectedIndex) {
+        final startIndex = index["start"];
+        final endIndex = index["end"];
+
+        result.add({
+          "text": content.substring(prevEndIndex, startIndex),
+          "isKeyword": false
+        });
+        result.add({
+          "text": content.substring(startIndex, endIndex),
+          "isKeyword": true
+        });
+        prevEndIndex = endIndex; // 이전 end index 저장
+      }
+
+      // 마지막 문자 저장
+      result.add({"text": content.substring(prevEndIndex), "isKeyword": false});
+
+      return result;
+    }
+
     List<InlineSpan> getSpanList() {
-      final data = findKeyword(content, keywords);
-      final spans = data
-          .map((e) => {
-                e["isKeyword"]
-                    ? WidgetSpan(
-                        child: SizedBox(
-                        width: 16.0 * e["text"].length,
-                        height: 26,
-                        child: TextField(
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: CustomTheme.themeData.primaryColor,
-                          ),
-                          onChanged: ((value) {
-                            // 입력값 저장
-                          }),
-                        ),
-                      ))
-                    : TextSpan(text: e["text"])
-              })
-          .toList();
+      List<InlineSpan> spanList = [];
+      final result = sliceText(content, selectedIndex);
 
-      // List<Set<InlineSpan>> 에서 Set 제거
-      final flattenedSpans = spans.expand((e) => e).toList();
+      // 텍스트 수만큼 길이 초기화
+      userAnswerList = List.filled(result.length, "", growable: true);
 
-      return flattenedSpans;
+      for (final element in result) {
+        final index = result.indexOf(element);
+
+        if (element["isKeyword"]) {
+          spanList.add(WidgetSpan(
+              child: SizedBox(
+            width: 18.0 * element["text"].length,
+            height: 26,
+            child: TextField(
+              style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400,
+                color: CustomTheme.themeData.primaryColor,
+              ),
+              onChanged: (value) {
+                userAnswerList[index] = value;
+              },
+            ),
+          )));
+        } else {
+          spanList.add(TextSpan(text: element["text"]));
+        }
+      }
+
+      return spanList;
     }
 
     return Scaffold(
@@ -137,10 +157,11 @@ class KeywordQuizScreen extends StatelessWidget {
             ),
             ElevatedButton(
               onPressed: () {
-                // 다음 빈칸 채우기
+                // 채점 결과 보여주기
+
                 // 다 풀었으면 문장 쓰기로 이동
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const SentenceQuizScreen()));
+                // Navigator.of(context).push(MaterialPageRoute(
+                //     builder: (context) => const SentenceQuizScreen()));
               },
               style: elevatedButtonStyle,
               child: const Text("제출"),
