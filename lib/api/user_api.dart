35ea26472a1d448bedb872377ebe6e69d128975e
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-04-23 23:02:46 +0900

    Feat: 토큰 재발급 추가

--- lib/api/user_api.dart
+++ lib/api/user_api.dart
@@ -22,20 +22,25 @@ class UserAPI {
     }
   }
 
-  static Future refreshToken(
-      {required String accessToken, required String refreshToken}) async {
-    final data = {
-      "accessToken": accessToken,
-      "refreshToken": refreshToken,
-      "grantType": "string"
-    };
-    final response = await http.post(
-        Uri.parse('${Constants.baseURL}/users/reissue'),
-        body: jsonEncode(data),
-        headers: {"Content-Type": "application/json"});
+  static Future reIssue() async {
+    final userInfo = await storage.read(key: "userInfo");
+    final refreshToken = jsonDecode(userInfo!)["refreshToken"];
+    final accessToken = jsonDecode(userInfo)["accessToken"];
+
+    final response = await http
+        .post(Uri.parse('${Constants.baseURL}/users/reissue'), headers: {
+      "Authorization": "Bearer $accessToken",
+      "RefreshToken": "Bearer $refreshToken",
+    });
 
     if (response.statusCode == 200) {
+      // storage 사용자 정보 업데이트
+      await storage.write(key: "userInfo", value: jsonEncode(response));
+
       return jsonDecode(utf8.decode(response.bodyBytes));
+    } else if (response.statusCode == 401) {
+      // storage에서 만료된 토큰 삭제
+      await storage.delete(key: "userInfo");
     } else {
       print('Error code: ${response.statusCode}');
       throw Exception('토큰을 재발급하지 못했습니다.');
@@ -50,7 +55,7 @@ class UserAPI {
     final response = await http
         .delete(Uri.parse('${Constants.baseURL}/users/reissue'), headers: {
       "Authorization": "Bearer $accessToken",
-      "RefreshToken": refreshToken
+      "RefreshToken": "Bearer $refreshToken"
     });
 
     if (response.statusCode == 200) {
@@ -58,6 +63,8 @@ class UserAPI {
       await storage.delete(key: "userInfo");
 
       return response;
+    } else if (response.statusCode == 401) {
+      await reIssue();
     } else {
       print('Error code: ${response.statusCode}');
       throw Exception('로그아웃 실패');
