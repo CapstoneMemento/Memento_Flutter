3c6be28a9d49ff54bbf8c7dfa9771b9bb983f13b
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-04-29 16:19:42 +0900

    chore: User 모델 수정

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/cupertino.dart';
 
 class User extends ChangeNotifier {
-  final String userId, nickname, accessToken, refreshToken;
+  final String userId, nickname, accessToken, refreshToken, expiration;
 
-  User(
-      {required this.userId,
-      required this.nickname,
-      required this.accessToken,
-      required this.refreshToken});
+  User({
+    required this.userId,
+    required this.nickname,
+    required this.accessToken,
+    required this.refreshToken,
+    required this.expiration,
+  });
 
   User.fromJson(Map json)
       : userId = json["userid"],
         nickname = json["nickname"],
         accessToken = json["accessToken"],
-        refreshToken = json["refreshToken"];
+        refreshToken = json["refreshToken"],
+        expiration = DateTime.now().add(const Duration(minutes: 28)).toString();
 }
