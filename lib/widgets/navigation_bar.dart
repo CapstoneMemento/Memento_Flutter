61569baaa27ff15a618551dc0689c08ab8d8d7d7
Yong-yeon Kim <70844774+lotus-yeon@users.noreply.github.com>
2023-05-14 21:55:24 +0900

    Chore: 로그인 만료 시, dialog 띄우기

--- lib/widgets/navigation_bar.dart
+++ lib/widgets/navigation_bar.dart
@@ -60,12 +60,30 @@ class _NavigationBarWidgetState extends State<NavigationBarWidget> {
         // refreshToken도 만료되면
         // Storage 사용자 정보 삭제
         await Storage.deleteData(key: "userInfo");
-        // 로그인 화면으로 이동
-        if (mounted) {
-          Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (context) => const LoginScreen()),
-              (route) => false);
-        }
+        // 로그인 화면으로 돌아가기
+        showDialog(
+            context: context,
+            builder: ((context) => AlertDialog(
+                  contentPadding: const EdgeInsets.all(16),
+                  title: Text(
+                    "로그인 만료",
+                    style: CustomTheme.themeData.textTheme.titleSmall,
+                  ),
+                  content: const Text("로그인이 만료되었습니다.\n다시 로그인 해주세요.",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 14, fontWeight: FontWeight.w400)),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (context) => const LoginScreen()),
+                              (route) => false);
+                        },
+                        child: const Text("확인"))
+                  ],
+                )));
       }
     });
   }
