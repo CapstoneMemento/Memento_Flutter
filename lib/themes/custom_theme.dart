f15133b68850b43ac39490dc23683ef880327d20
yyeonkim <seyeon.dev@gmail.com>
2023-05-16 13:10:59 +0900

    Style: CustomTheme 수정

--- lib/themes/custom_theme.dart
+++ lib/themes/custom_theme.dart
@@ -11,19 +11,19 @@ class CustomTheme {
           fontWeight: FontWeight.w700,
         ),
         titleMedium: TextStyle(
-          fontSize: 16,
+          fontSize: 18,
           fontWeight: FontWeight.w700,
         ),
         titleSmall: TextStyle(
-          fontSize: 14,
+          fontSize: 16,
           fontWeight: FontWeight.w700,
         ),
         bodyMedium: TextStyle(
-          fontSize: 14,
+          fontSize: 16,
           fontWeight: FontWeight.w400,
         ),
         bodySmall: TextStyle(
-            fontSize: 12,
+            fontSize: 14,
             fontWeight: FontWeight.w300,
             color: Color(0xff828282))),
   );
